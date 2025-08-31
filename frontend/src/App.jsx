26e44deaa3e00ce8e872dc3547d0f9a97c67aea2
RedShadowWalker <harsh.malpani23@spit.ac.in>
2025-08-31 14:06:55 +0530

    Resources

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
-import ReportIncident from "./pages/ReportIncident"
+// import ReportIncident from "./pages/ReportIncident";
+import Resources from './pages/Resources';
+
+// function App() {
+//   return (
+//     <div>
+//       <ReportIncident />
+//     </div>
+//   )
+// }
+
+// export default App
 
 function App() {
   return (
     <div>
-      <ReportIncident />
+      <Resources />
     </div>
   )
 }
